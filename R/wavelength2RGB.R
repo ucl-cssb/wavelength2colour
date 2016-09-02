6ec39875dc8c6719af6291f4dfc02ebc0e36ea2e
A J H Fedorec <ajfedorec@gmail.com>
2016-09-02 17:04:31 +0100

    Linting and correct attribution

--- R/wavelength2RGB.R
+++ R/wavelength2RGB.R
@@ -1,5 +1,7 @@
 #' Turn a wavelength (in nm) in to an RGB vector.
 #'
+#' Code adapted from \url{http://www.efg2.com/Lab/ScienceAndEngineering/Spectra.htm}
+#'
 #' @param wavelength a numerical wavelength to convert to RGB.
 #'
 #' @return a vector of three numeric values representing the RGB components.
@@ -7,43 +9,43 @@
 wavelength2RGB <- function(wavelength){
   # CONST
   gamma        <-   0.80
-  intensityMax <-   255
+  intensity_max <-   255
 
   adjust <- function(colour, factor){
-    if(colour == 0.0){
+    if (colour == 0.0){
       return(0)
     }
     else{
-      return(round(intensityMax * (colour * factor) ^ gamma))
+      return(round(intensity_max * (colour * factor) ^ gamma))
     }
   }
 
-  if(wavelength>=380 && wavelength<440){
-    red   <- -(wavelength - 440) / (440 - 380);
+  if (wavelength >= 380 && wavelength < 440){
+    red   <- - (wavelength - 440) / (440 - 380);
     green <- 0.0;
     blue  <- 1.0
   }
-  else if(wavelength>=440 && wavelength<490){
+  else if (wavelength >= 440 && wavelength < 490){
     red   <- 0.0;
     green <- (wavelength - 440) / (490 - 440);
     blue  <- 1.0
   }
-  else if(wavelength>=490 && wavelength<510){
+  else if (wavelength >= 490 && wavelength < 510){
     red   <- 0.0;
     green <- 1.0;
-    blue  <- -(wavelength - 510) / (510 - 490)
+    blue  <- - (wavelength - 510) / (510 - 490)
   }
-  else if(wavelength>=510 && wavelength<580){
+  else if (wavelength >= 510 && wavelength < 580){
     red   <- (wavelength - 510) / (580 - 510);
     green <- 1.0;
     blue  <- 0.0
   }
-  else if(wavelength>=580 && wavelength<645){
+  else if (wavelength >= 580 && wavelength < 645){
     red   <- 1.0;
-    green <- -(wavelength - 645) / (645 - 580);
+    green <- - (wavelength - 645) / (645 - 580);
     blue  <- 0.0
   }
-  else if(wavelength>=645 && wavelength<=780){
+  else if (wavelength >= 645 && wavelength <= 780){
     red   <- 1.0;
     green <- 0.0;
     blue  <- 0.0
@@ -54,14 +56,14 @@ wavelength2RGB <- function(wavelength){
     blue  <- 0.0
   }
   # Let the intensity fall off near the vision limits
-  if(wavelength>=380 && wavelength<420){
-    factor <- 0.3 + 0.7*(wavelength - 380) / (420 - 380);
+  if (wavelength >= 380 && wavelength < 420){
+    factor <- 0.3 + 0.7 * (wavelength - 380) / (420 - 380);
   }
-  else if(wavelength>=420 && wavelength<700){
+  else if (wavelength >= 420 && wavelength < 700){
     factor <- 1.0;
   }
-  else if(wavelength>=700 && wavelength<=780){
-    factor <- 0.3 + 0.7*(780 - wavelength) / (780 - 700)
+  else if (wavelength >= 700 && wavelength <= 780){
+    factor <- 0.3 + 0.7 * (780 - wavelength) / (780 - 700)
   }
   else{
     factor <- 0.0
--- R/wavelength2hex.R
+++ R/wavelength2hex.R
@@ -8,6 +8,6 @@
 #' @export
 wavelength2hex <- function(wavelength){
   RGB <- wavelength2RGB(wavelength)
-  hex <- rgb(RGB[1], RGB[2], RGB[3], maxColorValue = 255)
+  hex <- grDevices::rgb(RGB[1], RGB[2], RGB[3], maxColorValue = 255)
   return(hex)
 }
